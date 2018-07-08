c9b5ec7ca25861dad00dbf409965c53ec192eb8a
Marcell666 <brucemarcellino@hotmail.com>
2018-07-08 18:12:44 -0300

    fix bug rainha concatena listas

--- src/movimento/MRainha.java
+++ src/movimento/MRainha.java
@@ -7,13 +7,18 @@ public class MRainha extends MoveBase implements IMove{
 	@Override
 	public List<Integer> move(int[][] pecas, int x, int y) {
 		inicializa();
+		List<Integer> lstTorre;
+		List<Integer> lstBispo;
 		/*
 		 * O movimento da rainha é o movimento da torre e do bispo juntos
 		 * entao podemos usar as classe que ja escrevemos e concatenar os resultados
 		 */
 		
-		pos = new MTorre().move(pecas, x, y);
-		pos.addAll(0, new MBispo().move(pecas, x, y));
+		lstTorre = new MTorre().move(pecas, x, y);
+		lstBispo = new MBispo().move(pecas, x, y);
+		pos.addAll(lstTorre);
+		pos.addAll(lstBispo);
+	//	pos = new MBispo().move(pecas, x, y);
 		return finaliza();
 	}
 	@Override
